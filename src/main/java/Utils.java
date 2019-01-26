bae0f4636230f0222c0af45515c3ccf24a2bc351
Ashish Patel <ashishpatel0720@gmail.com>
2019-01-27 02:13:26 +0530

    MAVEN: added

---
+++ src/main/java/Utils.java
@@ -0,0 +1,8 @@
+public class Utils {
+
+    /* this function evaluates a string using javascript engine, syntax of expression shoule be  must be javascript*/
+    public static String evalute(String expression){
+
+
+    }
+}
